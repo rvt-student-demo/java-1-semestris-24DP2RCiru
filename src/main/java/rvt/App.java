683d98a895c2fe0b3775abd4fd2c2f66dd064612
PlatLV <1r.cirulis@gmail.com>
2025-11-03 16:58:24 +0200

    programma

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -2,7 +2,6 @@ package rvt;
 
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        
     }
 }
-12425236343463
---
+++ src/main/java/rvt/program.java
@@ -0,0 +1,22 @@
+package rvt;
+import java.util.Scanner;
+public class program {
+    public static void main(String[] args) {
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+
+        
+        
+        String message = scanner.nextLine();
+
+        
+        System.out.println(message);
+        System.out.println(message);
+        
+        scanner.close();
+    }
+    
+}
