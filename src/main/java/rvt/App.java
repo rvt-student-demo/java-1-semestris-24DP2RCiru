1b66d4a60bc5aeca83c2f5be505664cb2e4a85e9
PlatLV <1r.cirulis@gmail.com>
2025-11-03 17:29:49 +0200

    uptade price calculation

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,23 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int numberCount = 0;
+    int sum = 0;
+    while(true){
+        System.out.println("Give a number: ");
+        int number = scanner.nextInt();
+        if (number == 0) {
+        break;
+    }
+    numberCount++;
+    sum += number;
+}
+System.out.println("Number of numbers: " + numberCount);
+        System.out.println("Sum of numbers: " + sum);
+        scanner.close();
     }
 }
