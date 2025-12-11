ad28d30ae96977e3380884ae313ca70b3f5b744e
PlatLV <240304rc@rvt.lv>
2025-12-11 16:15:09 +0200

    functionality to collect user input until empty

---
+++ src/main/java/rvt/list_size.java
@@ -0,0 +1,33 @@
+package rvt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class list_size {
+
+
+    public static void main(String[] args) {
+        
+        
+    Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> names = new ArrayList<>();
+
+        while (true) {
+
+            String name = scanner.nextLine();
+
+            if (name.isEmpty()) {
+
+                break;
+            }
+
+            names.add(name);
+        }
+
+            scanner.close();
+
+            }
+
+    }
